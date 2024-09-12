08b80a206effc973a159ef5b88a89b56661e6ccd
Augustus Mocha <mocha4pf@gmail.com>
2024-09-12 21:45:01 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b){
+    return a + b;
+   
+}
+function subtract(a,b){
+    return a - b;
+
+}
+function multiply(a,b){
+    return a*b
+}
+function divide(a,b){
+    return a/b
+}
+function increment(n){
+    return n+=1;
+}
+function decrement(n){
+    return n-=1;
+}
+function makeInt(n){
+    return parseInt(n,10)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
+add(1001,2);
+subtract(124,2);
+multiply(681504,1)
+divide(1.5786802030456852,1)
+makeInt("770")
